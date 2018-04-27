166b8d22e64c112b9bc4459cf4fbd24c4d0e6894
jchaline <jchaline@@users.noreply.github.com>
2018-04-27 17:13:03 +0200

    test for configuration priorities

--- src/main/java/fr/arko/dokbuild/config/PropertiesConfiguration.java
+++ src/main/java/fr/arko/dokbuild/config/PropertiesConfiguration.java
@@ -14,13 +14,13 @@ public class PropertiesConfiguration {
 	@Bean
 	public PropertyPlaceholderConfigurer properties() {
 		final PropertyPlaceholderConfigurer ppc = new PropertyPlaceholderConfigurer();
-		ppc.setIgnoreResourceNotFound(true);
-		ppc.setIgnoreUnresolvablePlaceholders(true);
+		ppc.setIgnoreResourceNotFound(false);
+		ppc.setIgnoreUnresolvablePlaceholders(false);
 
 		final List<Resource> resourceLst = new ArrayList<Resource>();
 
 		resourceLst.add(new ClassPathResource("default.properties"));
-		resourceLst.add(new ClassPathResource("dokbuild_specific.properties"));
+		resourceLst.add(new ClassPathResource("application.properties"));
 
 		ppc.setLocations(resourceLst.toArray(new Resource[] {}));
 
