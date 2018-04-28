46d47455b9e4e1797c88050a7f731b5dc3ca75fd
jchaline <chaline.jeremy@gmail.com>
2018-04-28 11:57:00 +0200

    Correction for configuration

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # dokbuild
-toolbox pour dokkan battle, test avec sqlite
+toolbox for dokkan battle
+
+-- 
+1) run DokbuildLauncher
+2) src/main/resources should be in classpath
+3) any configuration can be override with external application.properties in classpath
--- src/main/java/fr/arko/dokbuild/config/PropertiesConfiguration.java
+++ src/main/java/fr/arko/dokbuild/config/PropertiesConfiguration.java
@@ -1,29 +1,10 @@
 package fr.arko.dokbuild.config;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @Configuration
+@PropertySources({@PropertySource("classpath:/default.properties"),@PropertySource(ignoreResourceNotFound=true, value="classpath:/application.properties")})
 public class PropertiesConfiguration {
-	@Bean
-	public PropertyPlaceholderConfigurer properties() {
-		final PropertyPlaceholderConfigurer ppc = new PropertyPlaceholderConfigurer();
-		ppc.setIgnoreResourceNotFound(false);
-		ppc.setIgnoreUnresolvablePlaceholders(false);
-
-		final List<Resource> resourceLst = new ArrayList<Resource>();
-
-		resourceLst.add(new ClassPathResource("default.properties"));
-		resourceLst.add(new ClassPathResource("application.properties"));
-
-		ppc.setLocations(resourceLst.toArray(new Resource[] {}));
-
-		return ppc;
-	}
 }
